9e47d38c26cbe3c73afd26dbfa3d2dd8b56a5565
denis <den.podgorod@gmail.com>
2026-04-06 13:46:13 +0300

    style: format code according to PEP 8 and fix project structure

--- error_handling/try_except_practice.py
+++ error_handling/try_except_practice.py
@@ -1,7 +1,7 @@
 try:
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
-    result = num1 / num2
+    resultы = num1 / num2
 except ZeroDivisionError:
     print("Error: Division by zero is not allowed.")
 except ValueError:
